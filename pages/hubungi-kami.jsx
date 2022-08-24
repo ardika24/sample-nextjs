5b7cd6007a4b9afb233da6eaedb5945175046afc
ardika24 <ardhieka24@gmail.com>
2022-08-24 19:28:38 +0700

    first commit

---
+++ pages/hubungi-kami.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import { Container } from "react-bootstrap";
+
+export default function TentangKami() {
+  <Container>
+    <Head>
+      <title>Tentang Kami</title>
+    </Head>
+
+    <h1>Tentang Kami</h1>
+  </Container>;
+}
---
+++ pages/tentang-kami.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import { Container } from "react-bootstrap";
+
+export default function HubungiKami() {
+  <Container>
+    <Head>
+      <title>Hubungi Kami</title>
+    </Head>
+
+    <h1>Hubungi Kami</h1>
+  </Container>;
+}
